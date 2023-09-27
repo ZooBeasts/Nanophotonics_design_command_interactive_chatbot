8fa480d4abaa198d2d2b4aa9af2502b08194ec5d
Ziheng Guo <75404784+ZooBeasts@users.noreply.github.com>
2023-09-27 22:19:13 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,129 +1,133 @@
-import numpy as np
 import random
 import json
+
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-from nltk_utils import bag_of_words, tokenize, stem
-from chat import NeuralNet
 
-
-with open('dict.json', 'r') as contents:
-    intents = json.load(contents)
-
-all_words = []
-tags = []
-xy = []
-# loop through each sentence in our intents patterns
-for intent in intents['intents']:
-    tag = intent['tag']
-    # add to tag list
-    tags.append(tag)
-    for pattern in intent['patterns']:
-        # tokenize each word in the sentence
-        w = tokenize(pattern)
-        # add to our words list
-        all_words.extend(w)
-        # add to xy pair
-        xy.append((w, tag))
-
-# stem and lower each word
-ignore_words = ['?', '.', '!']
-all_words = [stem(w) for w in all_words if w not in ignore_words]
-# remove duplicates and sort
-all_words = sorted(set(all_words))
-tags = sorted(set(tags))
-
-print(len(xy), "patterns")
-print(len(tags), "tags:", tags)
-print(len(all_words), "unique stemmed words:", all_words)
-
-# create training data
-X_train = []
-y_train = []
-for (pattern_sentence, tag) in xy:
-    # X: bag of words for each pattern_sentence
-    bag = bag_of_words(pattern_sentence, all_words)
-    X_train.append(bag)
-    # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
-    label = tags.index(tag)
-    y_train.append(label)
-
-X_train = np.array(X_train)
-y_train = np.array(y_train)
-
-# Hyper-parameters 
-num_epochs = 2000
-batch_size = 8
-learning_rate = 0.001
-input_size = len(X_train[0])
-hidden_size = 8
-output_size = len(tags)
-print(input_size, output_size)
-
-
-class ChatDataset(Dataset):
-
-    def __init__(self):
-        self.n_samples = len(X_train)
-        self.x_data = X_train
-        self.y_data = y_train
-
-    # support indexing such that dataset[i] can be used to get i-th sample
-    def __getitem__(self, index):
-        return self.x_data[index], self.y_data[index]
-
-    # we can call len(dataset) to return the size
-    def __len__(self):
-        return self.n_samples
-
-
-dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset,
-                          batch_size=batch_size,
-                          shuffle=True,
-                          num_workers=0)
+from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
+with open('dic.json', 'r') as json_data:
+    intents = json.load(json_data)
 
-# Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-# Train the model
-for epoch in range(num_epochs):
-    for (words, labels) in train_loader:
-        words = words.to(device)
-        labels = labels.to(dtype=torch.long).to(device)
+# Function to draw the bar graph
+def draw_bar_graph():
+    # Data for the bar graph (you can replace this with your own data)
+    categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4']
+    values = [10, 24, 15, 30]
 
-        # Forward pass
-        outputs = model(words)
-        # if y would be one-hot, we must apply
-        # labels = torch.max(labels, 1)[1]
-        loss = criterion(outputs, labels)
+    # Create a bar graph
+    fig, ax = plt.subplots()
+    ax.bar(categories, values)
+    ax.set_xlabel('Categories')
+    ax.set_ylabel('Values')
+    ax.set_title('Bar Graph')
 
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    # with open('data.csv', 'r') as file:
+    #     csv_reader = csv.reader(file)
+    #     next(csv_reader)  # Skip the header row if it exists
+    #
+    #     categories = []
+    #     values = []
+    #
+    #     for row in csv_reader:
+    #         categories.append(row[0])  # Assuming the first column contains category names
+    #         values.append(int(row[1]))  # Assuming the second column contains numerical values
+    #
+    #     # Create a bar graph
+    # fig, ax = plt.subplots()
+    # ax.bar(categories, values)
+    # ax.set_xlabel('Categories')
+    # ax.set_ylabel('Values')
+    # ax.set_title('Bar Graph')
 
-    if (epoch + 1) % 100 == 0:
-        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.7f}')
+    # Embed the Matplotlib plot into the Tkinter window
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas.get_tk_widget().pack()
 
-print(f'final loss: {loss.item():.7f}')
+# Create the main window
 
-data = {
-    "model_state": model.state_dict(),
-    "input_size": input_size,
-    "hidden_size": hidden_size,
-    "output_size": output_size,
-    "all_words": all_words,
-    "tags": tags
-}
+
+# start the event
+
+
+
+
+
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.l2 = nn.Linear(hidden_size, hidden_size)
+        self.l3 = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out)
+        out = self.relu(out)
+        out = self.l3(out)
+        return out
+
+
 
 FILE = "data.pth"
-torch.save(data, FILE)
+data = torch.load(FILE)
 
-print(f'training complete. file saved to {FILE}')
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Zoobeasts"
+while True:
+    # sentence = "how are you?"
+    sentence = input("You: ")
+    if sentence == "quit":
+        break
+    elif sentence == "transfer matrix method":
+        window = tk.Tk()
+        window.title("Bar Graph App")
+
+        # Create a button to trigger the bar graph drawing
+        draw_button = ttk.Button(window, text="Draw Bar Graph", command=draw_bar_graph)
+        draw_button.pack()
+
+        # Run the Tkinter main loop
+        window.mainloop()
+    else:
+        pass
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: Sorry, I am still learning, please come back later")
